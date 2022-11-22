387289f17b49a97a7968cddc09af27b0b7232e7b
Shivam-Katare <katare27451@gmail.com>
2022-11-22 19:57:46 +0530

    All files updated

---
+++ pages/Footer.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import Link from "next/link"
+
+function Footer() {
+  return (
+    <Fragment>
+      <p className='footer'>Made with ❤ by <a href="https://github.com/Shivam-Katare" className="footer-link"> Shivam Katare</a></p>
+    </Fragment>
+  );
+}
+export default Footer;
